86adc1ab157f079c4e02c50069f75bb94ab227f9
Andrew Stakhov <andrew.stakhov@gmail.com>
2024-01-05 12:48:49 -0500

    Initial

---
+++ src/ActuatorInjector/ActuatorInjector.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Management.Endpoint;
+using Steeltoe.Management.Endpoint.CloudFoundry;
+
+[assembly: HostingStartup(typeof(ActuatorInjector.ActuatorInjector))]
+
+namespace ActuatorInjector;
+public class ActuatorInjector : IHostingStartup, IStartupFilter
+{
+    public void Configure(IWebHostBuilder builder)
+    {
+        Console.WriteLine("Injecting Steeltoe");
+        builder.ConfigureServices(services => services.TryAddEnumerable(ServiceDescriptor.Singleton<IStartupFilter>(this)));
+
+        builder.ConfigureAppConfiguration(c => c.AddCloudFoundry());
+        builder.AddHypermediaActuator();
+        builder.AddThreadDumpActuator();
+        builder.AddHeapDumpActuator();
+        builder.AddDbMigrationsActuator();
+        builder.AddEnvActuator();
+        builder.AddInfoActuator();
+        builder.AddHealthActuator();
+        builder.AddLoggersActuator();
+        builder.AddTraceActuator();
+        builder.AddMappingsActuator();
+        builder.AddCloudFoundryActuator();
+        //builder.AddAllActuators();
+        // var config = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+        // var services = new ServiceCollection();
+        // services.AddSingleton<IConfiguration>(config);
+        // var actuatorRegistrations = services.AddAllActuators().Where(x => x.ServiceType != typeof(IConfiguration)).ToList();
+        //
+        // builder.ConfigureServices(c =>
+        // {
+        //     foreach (var serviceDescriptor in actuatorRegistrations)
+        //     {
+        //         Console.WriteLine($"{serviceDescriptor.ServiceType} {serviceDescriptor.ImplementationType} {serviceDescriptor.Lifetime}");
+        //         c.Add(serviceDescriptor);
+        //     }
+        // });
+    }
+
+    public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+    {
+        return app =>
+        {
+            app.UseRouting();
+            if (app.ApplicationServices.GetService<ICorsService>() != null)
+            {
+                app.UseCors("SteeltoeManagement");
+            }
+            app.UseCloudFoundrySecurity();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapAllActuators(null);
+            });
+            next(app);
+        };
+    }
+}
